169a9d459340997118011649cb47c8e043c294a7
Baptiste Gasser <baptiste.gasser@etu.univ-amu.fr>
2022-04-28 18:40:37 -0400

    Initial commit

---
+++ Source/TAnyDatatype/Private/TAnyDatatype.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TAnyDatatype.h"
+
+#define LOCTEXT_NAMESPACE "FTAnyDatatypeModule"
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FTAnyDatatypeModule, TAnyDatatype)
---
+++ Source/TAnyDatatype/Public/TAnyDatatype.h
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class FTAnyDatatypeModule : public IModuleInterface {};
